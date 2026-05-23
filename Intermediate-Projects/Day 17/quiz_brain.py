37ddd307c82e8df3b44e0e36be3a0ad2a6b945c9
Prakhar Singh Sijwali <prakharsijwaliofficial@gmail.com>
2026-05-23 12:56:51 +0530

    Implement QuizBrain class for quiz functionality

---
+++ Intermediate-Projects/Day 17/quiz_brain.py
@@ -0,0 +1,24 @@
+class QuizBrain:
+    def __init__(self, list):
+        self.question_number = 0
+        self.list = list
+        self.score = 0
+    def next_question(self):
+        current_question = self.list[self.question_number]
+        self.question_number += 1
+        ans = input(f"Q.{self.question_number}: {current_question.text}. (True/False)?:  ").lower()
+        self.check_answer(ans, current_question.answer) 
+    def still_has_question(self):
+        return len(self.list) > self.question_number
+    def check_answer(self, ans, correct_answer):
+        if ans == correct_answer.lower():
+            print("You got it right!!")
+            self.score += 1
+        else:
+            print("That's wrong.")
+        print(f"The correct answer is {correct_answer}")
+        print(f"Your current score is: {self.score }/{self.question_number}")
+        print("\n")
+       
+
+
